d5106e91339f60d25bded14469dee389761d6c48
mianumer116 <128652547+mianumer116@users.noreply.github.com>
2024-05-24 11:10:36 +0500

    c

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const http=require('http')
-const express=require('express');
-const app=express();
-const path=require('path')
-const server=http.createServer(app)
-const {Server}=require('socket.io')
-const io=new Server(server)
+const http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
+app.use(express.static(path.resolve("./public")));
 
-app.use(express.static(path.resolve("./public")))
+app.get('/', function(req, res) {
+    return res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
-app.get('/',function(req,res){
-    return res.sendFile('./public/index.html')
-})
+app.get('/umer', function(req, res) {
+    res.render('form'); // Assuming you have a template engine configured for rendering
+});
 
-app.get('/umer',function(req,res){
-    res.render('form')
-})
+// Socket.io
+io.on('connection', function(socket) {
+    socket.on('user-message', function(message) {
+        io.emit("message", message);
+    });
+});
 
-//Socket.io
-io.on('connection',function(socket){
+const port = process.env.PORT || 3000;
 
-    socket.on('user-message',function(message){
-       io.emit("message",message)
-    })
-
-})
-const port=process.env.PORT || 3000
-
-server.listen(3000,function(){
-    console.log("Server is listening on port 3000")
-})
+server.listen(port, function() {
+    console.log("Server is listening on port " + port);
+});
