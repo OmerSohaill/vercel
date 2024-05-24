b17d4336380e0aae62cff188605771732163d713
mianumer116 <128652547+mianumer116@users.noreply.github.com>
2024-05-24 10:58:00 +0500

    C

--- index.js
+++ index.js
@@ -1,63 +1,31 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables from .env file
+const http=require('http')
+const express=require('express');
+const app=express();
+const path=require('path')
+const server=http.createServer(app)
+const {Server}=require('socket.io')
+const io=new Server(server)
 
-const app = express();
-const port = process.env.PORT || 3000;
-const mongoURI = process.env.MONGO_URI;
 
-app.use(cors());
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(express.static(path.resolve("./public")))
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-const {sc}=require('./models/todo')
-// Connect to MongoDB
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+app.get('/',function(req,res){
+    return res.sendFile('./public/index.html')
+})
 
-app.get('/', (req, res) => {
-  try {
-    res.render('login');
-  } catch (error) {
-    res.send(error);
-  }
-});
+app.get('/umer',function(req,res){
+    res.render('form')
+})
 
-const data = [];
+//Socket.io
+io.on('connection',function(socket){
 
-app.post('/', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    data.push({ email, password });
-    const result=new sc({email,password})
-    results=await result.save();
-    if(results){
-     return res.send("Data save successfully")
-    }
-    else{
+    socket.on('user-message',function(message){
+       io.emit("message",message)
+    })
 
-      return res.end("Check your EMAIL AND PASSWORD AND TRY AGAIN");
-    }
-  } catch (error) {
-    res.send(error);
-  }
-});
-app.get('/data', (req, res) => {
-  try {
-    res.send(data);
-  } catch (error) {
-    res.send(error);
-  }
-});
+})
 
-app.listen(port, () => {
-  console.log('Server is listening on Port', port);
-});
+server.listen(3000,function(){
+    console.log("Server is listening on port 3000")
+})
